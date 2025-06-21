b34a74047e0531cc29f1b1a103bb3e7c18e0161b
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-06-21 10:53:53 -0600

    test(projects/08-mern-frontend): Setup tests suites on mern project

---
+++ projects/08-mern-frontend/jest.setup.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+import 'setimmediate'
+import { TextDecoder, TextEncoder } from 'util'
+import 'whatwg-fetch'
+
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+
+require('dotenv').config({
+  path: '.env.test'
+})
+
+jest.mock('./src/helpers/getEnvVariables.js', () => ({
+  getEnvVariables: () => ({ ...process.env })
+}))
