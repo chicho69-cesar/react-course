cf364c09ca79e456ff859a5095440b14be060eab
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-12 09:31:40 -0600

    feat(course/01-review-ts): Add review app

---
+++ course/01-review-ts/src/data/heroes.data.ts
@@ -0,0 +1,48 @@
+export interface Hero {
+  id: number
+  name: string
+  owner: Owner
+}
+
+// type Owner = 'DC' | 'Marvel'
+export const Owner = {
+  DC: 'DC',
+  Marvel: 'Marvel',
+} as const
+
+export type Owner = typeof Owner[keyof typeof Owner]
+
+export const heroes: Hero[] = [
+  {
+    id: 1,
+    name: 'Batman',
+    owner: Owner.DC,
+  },
+  {
+    id: 2,
+    name: 'Spiderman',
+    owner: Owner.Marvel,
+  },
+  {
+    id: 3,
+    name: 'Superman',
+    owner: Owner.DC,
+  },
+  {
+    id: 4,
+    name: 'Flash',
+    owner: Owner.DC,
+  },
+  {
+    id: 5,
+    name: 'Wolverine',
+    owner: Owner.Marvel,
+  },
+  {
+    id: 6,
+    name: 'Green Lantern',
+    owner: Owner.DC,
+  },
+]
+
+// export default heroes
--- course/01-review-ts/src/main.ts
+++ course/01-review-ts/src/main.ts
@@ -1,24 +1,25 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+// import "./bases/01-const-let"
+// import "./bases/02-template-string"
+// import "./bases/03-object-literal"
+// import "./bases/04-arrays"
+// import "./bases/05-functions"
+// import "./bases/06-obj-destructuring"
+// import "./bases/07-array-destructuring"
+// import "./bases/08-imp-exp"
+// import "./bases/09-promises"
+// import "./bases/10-fetch-api"
+import "./bases/11-async-await"
+
+// import { getHeroesByOwner } from "./bases/08-imp-exp"
+// import { Owner } from "./data/heroes.data"
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1>Hola Mundo</h1>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// En el main.ts usarán la función
+// console.log(getHeroesByOwner(Owner.Marvel))
