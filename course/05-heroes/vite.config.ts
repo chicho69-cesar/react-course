bd50bb5c06d7a6b78d4defb7841fe67d3955f8da
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-13 17:15:38 -0600

    feat(course/05-heroes): Configure heroes app

--- course/05-heroes/vite.config.ts
+++ course/05-heroes/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 })
